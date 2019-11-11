0e40956556fe8c82dc50acb24ed94177ce07a0ca
David Steeb <david.steeb@b13.com>
2019-11-11 13:35:09 +0100

    [TASK] Remove database query for non-existing field in v10

--- Classes/Utility/PageSpeedInsightsUtility.php
+++ Classes/Utility/PageSpeedInsightsUtility.php
@@ -171,9 +171,6 @@ class PageSpeedInsightsUtility
     {
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_pagespeedinsights_results');
 
-        $constraints = [
-            $queryBuilder->expr()->eq('t3ver_id', 0)
-        ];
         if ($pageId > 0) {
             $constraints[] = $queryBuilder->expr()->eq('page_id', $pageId);
         }
